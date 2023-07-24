5b8cc30f8bcd6a74bb1c81464271b8cd0bff8b73
Aung Kyaw Phyoe <akp.aungkyawphyoe7@gmail.com>
2023-07-24 21:22:16 +0630

    first commit

---
+++ TravelAppUI/DashboardViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  TravelAppUI
+//
+//  Created by Aung Kyaw Phyo on 7/24/23.
+//
+
+import UIKit
+
+class DashboardViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
---
+++ TravelAppUIUITests/TravelAppUIUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  TravelAppUIUITestsLaunchTests.swift
+//  TravelAppUIUITests
+//
+//  Created by Aung Kyaw Phyo on 7/24/23.
+//
+
+import XCTest
+
+final class TravelAppUIUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
